031a8e0fc067b6c3e2d36655e8378c4c8cbb1a69
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-04-11 09:51:42 +0530

    Time: 2 ms (71.88%) | Memory: 55.2 MB (36.29%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,11 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0;i<=nums.length;i++){
-        for(let j = i+1;j<=nums.length;j++){
-            if(nums[i] + nums[j] == target){
-                return [i,j]
-            }
-        }
+    const numMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
     }
+    numMap.set(nums[i], i);
+  }
+  return null;
 };
