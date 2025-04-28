303d147add2571d5a9d038b215144d778d11670b
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-04-29 01:32:08 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (93.28%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -3,10 +3,8 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let sum = 0
-    for(let i = 0; i<nums.length;i++){
-        sum += nums[i]
-        nums[i] = sum
+    for(let i = 1; i<nums.length;i++){
+        nums[i] += nums[i-1]
     }
     return nums
 };
