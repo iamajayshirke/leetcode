2ea69e41f95f89a83c6f38fec677eb3d37b280d3
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-04-30 23:56:42 +0530

    Time: 2 ms (69.86%) | Memory: 57.7 MB (55.04%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    const totalSum = nums.reduce((sum, num) => sum + num, 0);
+    let leftSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        const rightSum = totalSum - leftSum - nums[i];
+        if (leftSum === rightSum) {
+            return i;
+        }
+        leftSum += nums[i];
+    }
+
+    return -1;
+};
