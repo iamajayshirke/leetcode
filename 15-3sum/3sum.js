9b97a21cd032901b3887c01aa95dcb47b641cbde
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-05-05 23:24:37 +0530

    Time: 60 ms (20.01%) | Memory: 73.4 MB (17.00%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+   nums.sort((a,b) => a-b)
+    let triplets = []
+    for(let i = 0;i<nums.length;i++){
+         if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+        let j = i+1
+        let k = nums.length-1
+        while(j<k){
+            const sum = nums[i] + nums[j] + nums[k];
+
+      if (sum === 0) {
+        triplets.push([nums[i], nums[j], nums[k]]);
+
+        // Skip duplicate numbers for the second element
+        while (j < k && nums[j] === nums[j + 1]) {
+          j++;
+        }
+        // Skip duplicate numbers for the third element
+        while (j < k && nums[k] === nums[k - 1]) {
+          k--;
+        }
+
+        j++;
+        k--;
+      } else if (sum < 0) {
+        j++;
+      } else {
+        k--;
+      }
+        
+        }
+        
+    }
+    console.log(nums)
+    return triplets
+};
