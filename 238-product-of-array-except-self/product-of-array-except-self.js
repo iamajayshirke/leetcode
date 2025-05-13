e27cdf8673e481aff37efe54a3aa0533bb750d0f
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-05-14 00:09:32 +0530

    Time: 3 ms (92.79%) | Memory: 71.1 MB (48.72%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,20 +3,17 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let productLeft = []
-    let productRight = []
-    productLeft[0] = 1
-    for(let i = 1;i < nums.length;i++){
-        productLeft[i] = productLeft[i-1] * nums[i-1]
+    let n = nums.length;
+	let answer = new Array(n).fill(1);
+	let prev = 1;
+	for (let i = n-1; i >= 0; i--) {
+        answer[i] = answer[i] * prev;
+        prev = prev * nums[i];
     }
-    productRight[nums.length-1] = 1
-    for(let i = nums.length-2;i>=0;i--){
-        productRight[i] = productRight[i+1] * nums[i+1] 
+    prev = 1;
+    for (let i = 0; i < n; i++) {
+        answer[i] = answer[i] * prev;
+        prev = prev * nums[i];
     }
-    
-    for(let i = 0;i < nums.length;i++){
-        nums[i] = productLeft[i] * productRight[i]
-    }
-    
-    return nums
+    return answer;
 };
