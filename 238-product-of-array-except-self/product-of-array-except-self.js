40f6c3f4253a4386971c864ab620ec08b639854a
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-05-13 23:53:02 +0530

    Time: 42 ms (5.70%) | Memory: 72.7 MB (18.34%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let productLeft = []
+    let productRight = []
+    let ans = []
+    productLeft[0] = 1
+    for(let i = 1;i < nums.length;i++){
+        productLeft[i] = productLeft[i-1] * nums[i-1]
+    }
+    productRight[nums.length-1] = 1
+    for(let i = nums.length-2;i>=0;i--){
+        productRight[i] = productRight[i+1] * nums[i+1] 
+    }
+    
+    console.log(productRight,"Product Right")
+    for(let i = 0;i < nums.length;i++){
+        ans[i] = productLeft[i] * productRight[i]
+    }
+    
+    return ans
+};
