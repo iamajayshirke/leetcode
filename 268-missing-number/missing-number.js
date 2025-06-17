bed1d3db085f3a6bd93baf4e18ebc72400d948d9
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-06-17 23:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 56.9 MB (35.08%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -2,11 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-    for(let i = 0;i<nums.length;i++){
-    if(!nums.includes(i)){
-        return i
+var missingNumber = function (nums) {
+    let n = nums.length
+    let sum = n * (n + 1) / 2
+    let prefixSum = 0
+    for (let i = 0; i < nums.length; i++) {
+        prefixSum += nums[i]
     }
-}
-return (nums.length)
+    
+        return sum - prefixSum
 };
