e8e96b2d99d2a614814e64fbcd2d43d516b9419a
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-06-19 23:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 57.2 MB (40.93%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -4,7 +4,8 @@
  */
 var isPowerOfTwo = function(n) {
     if(n == 1) return true
-    if(n<1) return false
+    if(n%2 !== 0) return false
+    if(n < 1) return false
 
     return isPowerOfTwo(n/2)
 };
