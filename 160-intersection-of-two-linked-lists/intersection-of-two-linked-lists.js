4ecba87c3cb8d8c2954a3e6d89788a05e91b6a39
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-07-04 10:15:55 +0530

    Time: 55 ms (70.94%) | Memory: 64.2 MB (30.73%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -13,19 +13,17 @@
  */
 var getIntersectionNode = function(headA, headB) {
     let hash = new Map()
-    let curr1 = headA
-    let curr2 = headB
-    while(curr1){
-        if(!hash.has(curr1)){
-            hash.set(curr1)
+    while(headA){
+        if(!hash.has(headA)){
+            hash.set(headA)
         }
-        curr1 = curr1.next
+        headA = headA.next
     }
-    while(curr2){
-        if(hash.has(curr2)){
-            return curr2
+    while(headB){
+        if(hash.has(headB)){
+            return headB
         }
-        curr2 = curr2.next
+        headB = headB.next
     }
     return null
 };
