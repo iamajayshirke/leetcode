55e6d9cdedc3b3b2b12807c622a32418b0a3e335
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-07-10 22:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (64.11%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,13 +5,10 @@
 var lengthOfLastWord = function(s) {
     let count = 0
     for(let i = s.length-1;i>=0;i--){
-        
-        if(s[i] == " " && count == 0) continue
-        if((s[i] == " ") && (count > 0)){
-            return count
-        }
-        if((s[i] =! "") || (s[i] =! " ")){
+        if(s[i] != " "){
             count+=1
+        }else if(count>0){
+            return count
         }
     }
     return count
