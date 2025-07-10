3b902b5a0389cc9599da0459220f98682008b300
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-07-10 23:20:34 +0530

    Time: 78 ms (5.42%) | Memory: 64.7 MB (5.19%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -7,11 +7,8 @@ var findWordsContaining = function(words, x) {
     let res = []
     for(let i = 0;i<words.length;i++){
         console.log(words[i])
-        for(let j = 0;j< words[i].length;j++){
-            if(words[i][j] == x){
-                res.push(i)
-                break
-            }
+        if(words[i].includes(x)){
+             res.push(i)
         }
     }
     return res
