017b9214aa165097f389cc7a637e5e9c0950cc53
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-07-10 11:37:12 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (62.48%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -10,26 +10,14 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    
     if(!head || !head.next) return head
+    let l = head
+    let r = head.next
 
-    // let sentinel = new ListNode()
-    // let prev = sentinel
-    let curr = head
-    let next = head.next
     
 
-    while(curr && next){
-        let temp = curr.val
-        curr.val = next.val
-        next.val =temp
-        // prev.next = next
-        // curr.next = next.next
-        // next.next = curr
+    l.next = swapPairs(r.next)
+    r.next = l
 
-        // prev = curr
-        curr = curr.next.next
-        next = next.next && next.next.next
-    }
-    return head
+    return r
 };
