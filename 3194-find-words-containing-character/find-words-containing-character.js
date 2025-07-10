65acc5114acbdb969ecfdcf0eaaca4e2fe4b11cd
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-07-10 23:05:47 +0530

    Time: 78 ms (5.42%) | Memory: 64.3 MB (5.19%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    let res = []
+    for(let i = 0;i<words.length;i++){
+        console.log(words[i])
+        for(let j = 0;j< words[i].length;j++){
+            if(words[i][j] == x){
+                res.push(i)
+                break
+            }
+        }
+    }
+    return res
+};
