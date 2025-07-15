cd2a32bee65668a399d1fdc600bc306e37198067
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-07-15 19:21:15 +0530

    Time: 14 ms (9.17%) | Memory: 63.5 MB (8.80%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -5,15 +5,24 @@
 
 
 var isPalindrome = function(s) {
-     let filStr = ""
-    let revStr = ""
-    let rev = ""
+    
+    let filStr = ""
     s = s.toLowerCase()
     for (let i = 0;i< s.length;i++){
         if(s[i].match(/[a-z0-9]/i)){
             filStr += s[i]
-            rev = s[i] + rev
         }
     }
-    return rev == filStr
+    if(filStr.length == 0) return true
+    let start = 0
+    let end = filStr.length-1
+    while(start <= end){
+        if(start == end){
+            return true
+        }
+        if(filStr[start] !== filStr[end]) return false
+        start++
+        end--
+    }
+    return true
 };
