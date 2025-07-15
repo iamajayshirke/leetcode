42d574e17a24e1ecff5e6d97a99101a3239a8326
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-07-15 19:47:29 +0530

    Time: 6 ms (49.15%) | Memory: 61.8 MB (14.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -6,23 +6,26 @@
 
 var isPalindrome = function(s) {
     
-    let filStr = ""
-    s = s.toLowerCase()
-    for (let i = 0;i< s.length;i++){
-        if(s[i].match(/[a-z0-9]/i)){
-            filStr += s[i]
-        }
-    }
-    if(filStr.length == 0) return true
+     s = s.toLowerCase()
     let start = 0
-    let end = filStr.length-1
-    while(start <= end){
-        if(start == end){
-            return true
+    let end = s.length - 1
+    
+    while(start<=end){
+       
+        if(!s[start].match(/[a-z0-9]/i)){
+            start++
+            continue
+        } 
+        if(!s[end].match(/[a-z0-9]/i)){
+            end-- 
+            continue
+        } 
+        if(s[start] == s[end]){
+            start++
+            end--
+        }else{
+            return false
         }
-        if(filStr[start] !== filStr[end]) return false
-        start++
-        end--
     }
     return true
 };
