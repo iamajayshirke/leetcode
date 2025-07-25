a7a5b57a0b1e25dda08f68a8bd878258a4fbb1ca
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-07-25 23:50:39 +0530

    Time: 572 ms (5.27%) | Memory: 84.6 MB (5.10%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let hash = {}
+    let hash = new Map()
     for(let i = 0;i<nums.length;i++){
         console.log(nums[i])
-        if(!hash[nums[i]]){
-            hash[nums[i]] = 1
+        if(!hash.has(nums[i])){
+            hash.set(nums[i])
         }else{
             return true
         }
