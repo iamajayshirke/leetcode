c6ac23050c10f91844a5a492619018e55826db98
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-08-15 23:41:35 +0530

    Time: 5 ms (38.48%) | Memory: 57.4 MB (53.11%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,33 +4,30 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(find, arr) {
-    const stack = [];
-  const ngeMap = new Map();
-
-  // Iterate through the array from right to left
-  for (let i = arr.length - 1; i >= 0; i--) {
-    // Pop all elements from the stack that are smaller than the current element
-    while (stack.length > 0 && stack[stack.length - 1] <= arr[i]) {
-      stack.pop();
+let hash = {}
+    let stack = []
+    for(let i=arr.length-1;i>=0;i--){
+        if(arr.length-1 == i){
+            hash[arr[i]] = -1
+        }else if(arr[i] < stack[stack.length-1]){
+            hash[arr[i]] = stack[stack.length-1]
+        }else{
+            while(stack.length){
+                if(arr[i] > stack[stack.length-1]){
+                    stack.pop()
+                }else{
+                    hash[arr[i]] = stack[stack.length-1]
+                    break
+                }
+                hash[arr[i]] = -1
+                // break
+            }
+        }
+        // if(arr[i] < stack[stack.length-1] || arr.length-1 == i){
+            stack.push(arr[i])
+        // }
+        
+        // console.log(stack, hash,stack[stack.length-1])
     }
-
-    // After the while loop, if the stack is empty, there is no NGE
-    // otherwise, the top of the stack is the NGE
-    if (stack.length === 0) {
-      ngeMap.set(arr[i], -1);
-    } else {
-      ngeMap.set(arr[i], stack[stack.length - 1]);
-    }
-
-    // Push the current element onto the stack
-    stack.push(arr[i]);
-  }
-
-  const result = [];
-  // Look up the NGE for each number in the 'find' array
-  for (let num of find) {
-    result.push(ngeMap.get(num));
-  }
-
-  return result;
+    return find.map((e)=> hash[e])
 };
