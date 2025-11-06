73b2d3e178a58ecf9010947b2dfef1e1d9d63231
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-11-07 00:53:28 +0530

    Time: 55 ms (69.38%) | Memory: 59.6 MB (42.52%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,12 +11,20 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let hash = new Map()
-    let curr = head
-    while(curr){
-        if(hash.has(curr)) return true
-        hash.set(curr)
-        curr = curr.next
+    if (!head || !head.next) {
+        return false;
     }
-    return false
+
+    let slow = head;
+    let fast = head.next;
+
+    while (fast && fast.next) {
+        if (slow === fast) {
+            return true; // Pointers meet - cycle detected
+        }
+        slow = slow.next;       // Moves 1 step
+        fast = fast.next.next;  // Moves 2 steps
+    }
+
+    return false;
 };
