ae18e125479a355dc941786e2236965ce6e32f21
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2025-11-12 00:49:52 +0530

    Time: 107 ms (3.85%) | Memory: 65.2 MB (6.23%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -10,33 +10,43 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    let merge = new ListNode()
-    let mergeStart = merge
-    while(list1 && list2){
-        if(list1.val >= list2.val){
-            let newNode = new ListNode(list2.val)
-            merge.next = newNode
-            merge = merge.next
-            list2 = list2.next
-        }else if(list1.val <= list2.val){
-            let newNode = new ListNode(list1.val)
-            merge.next = newNode
-            merge = merge.next
-            list1 = list1.next
+var mergeTwoLists = function(l1, l2) {
+    if(!l1 ) return l2
+    if(!l2 ) return l1
+    //Loop Both at Once till length of l1 and l2
+    let finalList = new ListNode(0)
+    let finalStart = finalList
+
+    while(l1 && l2){
+        
+        const l1Value = l1.val
+        const l2Value = l2.val
+console.log(l1,l2,l1.val,l2.val,l1Value,l2Value)
+        if(l1Value <= l2Value){
+            console.log(l1Value,l2Value,"ebter")
+            finalList.next = new ListNode(l1Value)
+            finalList = finalList.next
+            l1 = l1.next
+        }else{
+            console.log(l1Value,l2Value,"ebter2")
+            finalList.next = new ListNode(l2Value)
+            finalList = finalList.next
+            l2 = l2.next
         }
+        
     }
-    while(list1){
-        let newNode = new ListNode(list1.val)
-        merge.next = newNode
-        merge = merge.next
-        list1 = list1.next
+    console.log(l1,l2)
+    while(l1 ){
+        finalList.next = new ListNode(l1.val)
+        finalList = finalList.next
+        l1 = l1.next
     }
-    while(list2){
-        let newNode = new ListNode(list2.val)
-        merge.next = newNode
-        merge = merge.next
-        list2 = list2.next
+
+    
+    while(l2){
+        finalList.next = new ListNode(l2.val)
+        finalList = finalList.next
+        l2 = l2.next
     }
-    return mergeStart.next
+    return finalStart.next
 };
