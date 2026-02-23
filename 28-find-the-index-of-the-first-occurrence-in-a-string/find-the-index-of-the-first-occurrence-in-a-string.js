15db4823ce60001506c7e27b57818fdc38f83fca
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2026-02-23 19:38:45 +0530

    Time: 383 ms (5.07%) | Memory: 63.9 MB (5.21%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,6 +3,23 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
+var strStr = function(h, n) {
+     let wndw = n.length
+    let ptr = 0
+    while(ptr < h.length){
+        let count = 0
+        for(let j = 0;j<wndw;j++){
+            if(h[ptr+j] == n[j]){
+                count++
+            }else{
+                break
+            }
+        }
+        console.log(count,h[ptr])
+        if(count == wndw){
+            return ptr
+        }
+        ptr++
+    }
+    return -1
 };
