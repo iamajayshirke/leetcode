5ae1707ee6c22ae185a0235bf84574ca1f6740e7
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2026-04-23 23:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (5.82%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,11 +4,18 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-     let counter = 0
-    for (let i = 0; i<nums.length;i++){
-        if(nums[i] !== val){
-            nums[counter++] = nums[i];
+    let pointer = 0
+    for(let i = 0;i<nums.length;i++){
+        if(nums[i] !== val){ // 3 !== 3
+            nums[pointer] = nums[i]
+            pointer+=1
+        }else{
+            nums[pointer] = nums[i]
+            
         }
     }
-    return counter
+    return pointer
 };
+
+// 3 2 2 3
+// 2 2 _ _
