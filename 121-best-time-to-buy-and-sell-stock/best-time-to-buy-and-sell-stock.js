f913b767214e946ec177d6410f7abc578011d178
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2026-04-29 20:58:08 +0530

    Time: 1540 ms (5.05%) | Memory: 74.7 MB (5.54%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -2,16 +2,17 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    let minPrice = Infinity;
-  let maxProfit = 0;
-
-  for(let i = 0;i<prices.length;i++){
-    if(minPrice>prices[i]){
-        minPrice = prices[i]
+var maxProfit = function (prices) {
+    let min = Infinity
+    let maxProfit = 0
+    for(let i = 0;i<prices.length;i++){
+        if(min>prices[i]){
+            min = prices[i]                                 //1
+        }
+        let currentProfit = prices[i] - min < 0 ? 0:prices[i] - min
+        if(currentProfit > maxProfit)  maxProfit = currentProfit
+        console.log(maxProfit,min) 
     }
-    maxProfit = maxProfit > prices[i] - minPrice ? maxProfit : prices[i] - minPrice 
-  }
-
-  return maxProfit;
+    console.log(maxProfit)
+    return maxProfit
 };
