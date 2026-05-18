66f08739efe0cdda7fac3afcbb7e25f384a8ed92
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2026-05-18 16:58:30 +0530

    Time: 443 ms (5.06%) | Memory: 57.3 MB (6.77%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,17 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let firstEl = 0
-    let lastEl = numbers.length-1
     for(let i = 0;i<numbers.length;i++){
-        let sum = numbers[firstEl] + numbers[lastEl]
-        if(sum == target){
-            return [firstEl+1,lastEl+1]
-        }
-        if(sum>target){
-            lastEl-=1
-        }else{
-            firstEl+=1
+        for(let j = i+1;j<numbers.length;j++){
+            if(numbers[i] + numbers[j] === target){
+                return [i+1,j+1]
+            }
         }
     }
 };
