9fa1c92e562ef807c662b22c56f52e12b7d34456
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2026-05-31 14:27:27 +0530

    Time: 39 ms (43.64%) | Memory: 77.4 MB (5.35%) - LeetSync

--- 15-3sum/3sum.ts
+++ 15-3sum/3sum.ts
@@ -1,5 +1,4 @@
 const twoSum = (num: number[], index: number, target: number): number[][] => {
-    // Note: Array sorting is handled in threeSum, so we don't necessarily need to sort it again here.
     let left = index;
     let right = num.length - 1;
     let pairs:number[][] = []
@@ -18,23 +17,18 @@ const twoSum = (num: number[], index: number, target: number): number[][] => {
             right -= 1;
         }
     }
-    
-    // Fix 2: Provide a fallback return statement if no match is found
     return pairs; 
 };
 
-// Fix 1: Changed return type from : void to : number[][]
 const threeSum = (nums: number[]): number[][] => {
     nums.sort((a, b) => a - b);
     let trips: number[][] = [];
 
     for (let i = 0; i < nums.length - 2; i++) {
-        // Fix 3: Skip duplicate values for the first element to avoid identical triplets
         if (i > 0 && nums[i] === nums[i - 1]) continue;
 
         let twoSumAns = twoSum(nums, i + 1, -nums[i]);
 
-        // Check if a valid pair was actually found (array length will be 2)
         for(let pair of twoSumAns){
             trips.push([nums[i],...pair])
         }
