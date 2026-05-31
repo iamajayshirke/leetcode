22b7f081ae3528f7ff71bb05e55376eaf36c7312
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2026-05-31 23:50:26 +0530

    Time: 7 ms (20.82%) | Memory: 57.2 MB (10.77%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,14 +1,41 @@
 function strStr(haystack: string, needle: string): number {
-    let need = 0
-    for(let i = 0;i<haystack.length;i++){
-        if(haystack[i] == needle[need]){
-            need+=1
-            if(need === needle.length){
-                return i+1 - need
-            }
+    let h = haystack.length
+    let n = needle.length
+    let lps: number[] = [0]
+    let i = 0
+    let j = 1
+    // Build LPS Array
+    while(j < n){
+        if(needle[i] == needle[j]){
+            lps[j] = i+1
+            i+=1
+            j+=1
         }else{
-            i = i - need;
-            need=0
+            if(i == 0){
+                lps[j] = 0
+                j+=1
+            }else{
+                i = lps[i - 1]
+            }
+        }
+    }
+    console.log(lps,"LPS Array")
+    i = j = 0
+    //Compare Needle with HayStack
+    while(j < h){
+        //If needle is equals to haystack
+        if(needle[i] == haystack[j]){
+            ++i
+            ++j
+        }else{
+            if(i == 0){
+                j++
+            }else{
+                i = lps[i-1]
+            }
+        }
+        if(i == n){
+            return j - n
         }
     }
     return -1
