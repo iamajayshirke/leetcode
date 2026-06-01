be337de4645016593c49ad2672ade63c9b97261f
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2026-06-01 23:57:42 +0530

    Time: 88 ms (5.24%) | Memory: 64 MB (5.07%) - LeetSync

---
+++ 202-happy-number/happy-number.ts
@@ -0,0 +1,21 @@
+function isHappy(n: number): boolean {
+      let temp = getDigit(n)
+    for(let i = 0;i<100;i++){
+        console.log(temp)
+        if(temp == 1){
+            return true
+        }else{
+            temp = getDigit(temp)
+        }
+    }
+    return false
+};
+function getDigit(num:number):number{
+    let sum = 0
+    while(num >= 1){
+        let temp = Math.floor(num % 10)
+        sum+=temp*temp
+        num/=10
+    }
+    return sum
+}
