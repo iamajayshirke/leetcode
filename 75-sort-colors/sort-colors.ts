9c0e30f79aaaf886a3f861cb4551fed5f7e236f6
Ajay Shirke <65479415+iamajayshirke@users.noreply.github.com>
2026-06-05 22:01:41 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (61.03%) - LeetSync

--- 75-sort-colors/sort-colors.ts
+++ 75-sort-colors/sort-colors.ts
@@ -1,3 +1,18 @@
-function sortColors(nums: number[]): number[] {
-    return nums.sort((a,b)=> a-b)
+function sortColors(colors: number[]): number[] {
+let left = 0
+let mid = 0
+let right = colors.length-1
+while(mid <= right){
+    if(colors[mid] === 0){
+           [colors[left],colors[mid]] = [colors[mid],colors[left]]
+            left++
+            mid++
+       }else if(colors[mid] === 1){
+            mid++
+        }else if(colors[mid] === 2){
+            [colors[mid],colors[right]] = [colors[right],colors[mid]]
+            right--
+        }
+}
+return colors
 };
